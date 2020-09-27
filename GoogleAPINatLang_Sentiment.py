79ebf9b25d53b1d664a00f61ecceae3aae6fc3c5
Poras Shroff <porasshroff@gmail.com>
2020-09-27 16:00:37 -0400

    Update GoogleAPINatLang_Sentiment.py

--- GoogleAPINatLang_Sentiment.py
+++ GoogleAPINatLang_Sentiment.py
@@ -1,6 +1,38 @@
-
 from google.cloud import language
+from google.cloud.language import enums, types
 
-client=language.LanguageServiceClient()
-annotate=client.analyze_sentiment("hello this is a great time to run")
-print_result(annotate)
+
+#Authentication import
+from gcloud.aio.auth import IamClient
+from gcloud.rest.auth import Token
+client = IamClient()
+pubkeys = await client.list_public_keys()
+token = Token()
+print(token.get())
+print("-----------------")
+
+
+
+def analyze_text_sentiment(text):
+    client = language.LanguageServiceClient()
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    response = client.analyze_sentiment(document=document)
+
+    sentiment = response.document_sentiment
+    results = [
+        ('text', text),
+        ('score', sentiment.score),
+        ('magnitude', sentiment.magnitude),
+    ]
+    for k, v in results:
+        print('{:10}: {}'.format(k, v))
+
+text = "Hello today is a great and sunny day"
+analyze_text_sentiment(text)
+
+
+
+
